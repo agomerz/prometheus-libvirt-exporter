0f247b9fe4d4b5bae96579ff83b4f959f0d4d7e2
Andy Gomez <agomez@cytracom.com>
2025-04-07 19:50:29 -0400

    Replaced Value.UL with Value.I since in this version TypedParamValue uses an int64 field named 'I' Added uint64 cast to handle the values properly Rest of the VCPU stats collection logic remains unchanged

--- prometheus-libvirt-exporter.go
+++ prometheus-libvirt-exporter.go
@@ -371,7 +371,7 @@ func CollectDomain(ch chan<- prometheus.Metric, l *libvirt.Libvirt, domain domai
                     ch <- prometheus.MustNewConstMetric(
                         libvirtDomainVcpuWaitDesc,
                         prometheus.CounterValue,
-                        float64(param.Value.Ullong)/1e9,
+                        float64(uint64(param.Value.I)),
                         domain.domainName,
                         strconv.Itoa(i),
                     )
@@ -379,7 +379,7 @@ func CollectDomain(ch chan<- prometheus.Metric, l *libvirt.Libvirt, domain domai
                     ch <- prometheus.MustNewConstMetric(
                         libvirtDomainVcpuDelayDesc,
                         prometheus.CounterValue,
-                        float64(param.Value.Ullong)/1e9,
+                        float64(uint64(param.Value.I)),
                         domain.domainName,
                         strconv.Itoa(i),
                     )
